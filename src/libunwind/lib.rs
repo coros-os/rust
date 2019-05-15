57cf5eabfda7e4a3182559159f98b95bf9085826
Jeremy Soller <jeremy@system76.com>
2019-05-15 11:52:26 -0600

    Add gcc link when using crt-static

--- src/libunwind/lib.rs
+++ src/libunwind/lib.rs
@@ -31,6 +31,7 @@ cfg_if! {
 extern {}
 
 #[cfg(target_os = "redox")]
+#[link(name = "gcc", kind = "static-nobundle", cfg(target_feature = "crt-static"))]
 #[link(name = "gcc_eh", kind = "static-nobundle", cfg(target_feature = "crt-static"))]
 #[link(name = "gcc_s", cfg(not(target_feature = "crt-static")))]
 extern {}
